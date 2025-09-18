16f8c0ba0da1590eddcadc82b4b7e95b5de424c1
Anujbansal527 <you@example.com>
2025-09-18 22:39:30 +0530

    Making Our Own Loggers

---
+++ 03_loggers/logger.js
@@ -0,0 +1,28 @@
+import { json } from "express";
+import { createLogger,format,transports } from "winston";
+const { combine, timestamp, printf, colorize } = format;
+
+// Define custom log format
+const consoleLogFormat = format.combine(
+    format.colorize(),
+    format.printf(({ level, message, timestamp }) => {
+        return `${timestamp} [${level}]: ${message}`;
+    })
+);
+
+// Create a Winston instance
+const logger = createLogger({
+    level: 'info',
+    format: combine(
+        timestamp(),
+        json()
+    ),
+    transports: [
+        new transports.Console({
+            format: consoleLogFormat
+        }),
+        new transports.File({ filename: 'logs/error.log'}),
+    ],
+});
+
+export default logger;
